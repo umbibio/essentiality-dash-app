89ba887060e2ff110adde245f65a7426ab9f4965
qudseen <115419208+qudseen@users.noreply.github.com>
2023-11-21 14:09:36 -0500

    Update app.py

--- dash-app/app.py
+++ dash-app/app.py
@@ -2,7 +2,7 @@ from dash import Dash,html
 from dash_bootstrap_components.themes import BOOTSTRAP
 import dash_bootstrap_components as dbc
 
-url_base_pathname = '/test/'
+url_base_pathname = '/PkEssenDB/'
 
 theme="spacelab"
 external_stylesheets = [
